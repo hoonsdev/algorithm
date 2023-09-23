c3527beac3453149da1390de5fde54b077742c63
hoonsdev <123625444+hoonsdev@users.noreply.github.com>
2023-09-23 17:23:03 +0900

    [Bronze III] Title: 부호, Time: 408 ms, Memory: 69584 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.js
@@ -0,0 +1,24 @@
+const sum = () => {
+  const input = require("fs")
+    .readFileSync("/dev/stdin")
+    .toString()
+    .trim()
+    .split("\n")
+    .map(BigInt);
+  const len = input.length;
+  let ans = [];
+  for (let i = 0; i < len; i++) {
+    const n = Number(input[i]);
+    let sum = BigInt(0);
+    i++;
+    for (let j = 0; j < n; j++) {
+      sum += BigInt(input[i]);
+      i++;
+    }
+    i--;
+    ans.push(sum < 0 ? "-" : sum > 0 ? "+" : "0");
+  }
+  console.log(ans.join("\n"));
+};
+
+sum();
