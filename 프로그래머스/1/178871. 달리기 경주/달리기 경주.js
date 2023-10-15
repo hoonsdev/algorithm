5d06cd0e73bc45c04adfdf11b5496d5be0b7b570
hoonsdev <123625444+hoonsdev@users.noreply.github.com>
2023-10-16 02:12:24 +0900

    [level 1] Title: 달리기 경주, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,23 +1,17 @@
 function solution(players, callings) {
-  const playerMap = {};
-
-  // calling 원소들의 idx를 미리 구해서 저장
-  for (let i = 0; i < players.length; i++) {
-    playerMap[players[i]] = i;
-  }
-
-  for (let i = 0; i < callings.length; i++) {
-    const idx = playerMap[callings[i]];
-    const temp = players[idx - 1];
-
-    // 해당 idx와 이전 idx의 원소를 swap
-    players[idx - 1] = callings[i];
-    players[idx] = temp;
-
-    // map의 idx도 갱신
-    playerMap[callings[i]] = idx - 1;
-    playerMap[temp] = idx;
-  }
-
+  let playerMap = new Map();
+  players.forEach((player, idx) => {
+    playerMap.set(player, idx);
+  });
+  callings.forEach((calling) => {
+    let playerIdx = playerMap.get(calling);
+    if (playerIdx !== 0) {
+      let temp = players[playerIdx];
+      playerMap.set(calling, playerIdx - 1);
+      playerMap.set(players[playerIdx - 1], playerIdx);
+      players[playerIdx] = players[playerIdx - 1];
+      players[playerIdx - 1] = temp;
+    }
+  });
   return players;
 }
