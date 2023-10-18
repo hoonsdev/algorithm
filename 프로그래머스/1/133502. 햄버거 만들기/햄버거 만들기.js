b62e6af7141bb63705580c700804251c8aa1ed3a
hoonsdev <123625444+hoonsdev@users.noreply.github.com>
2023-10-18 10:34:28 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -5,10 +5,7 @@ function solution(ingredient) {
     result.push(ingredient[i]);
     // result 배열에서 순서 맞는지 체크
     if (result.slice(-4)?.join('') === '1231') {
-      result.pop();
-      result.pop();
-      result.pop();
-      result.pop();
+      result.splice(-4);
       answer++;
     }
   }
