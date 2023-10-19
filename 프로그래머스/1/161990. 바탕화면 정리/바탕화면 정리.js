b8dfebed64c473031fafbff428e48d850f16075a
hoonsdev <123625444+hoonsdev@users.noreply.github.com>
2023-10-19 22:51:17 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.44 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,17 @@
+function solution(wallpaper) {
+  var answer = [];
+  let X = [];
+  let Y = [];
+  for (let i = 0; i < wallpaper.length; i++) {
+    for (let j = 0; j < wallpaper[0].length; j++) {
+      if (wallpaper[i][j] === '#') {
+        X.push(i);
+        Y.push(j);
+      }
+    }
+  }
+  X.sort((a, b) => a - b);
+  Y.sort((a, b) => a - b);
+  answer.push(X[0], Y[0], X[X.length - 1] + 1, Y[Y.length - 1] + 1);
+  return answer;
+}
