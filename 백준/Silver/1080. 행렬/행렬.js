ba4ec1bcbb9c32005cc095425b6574159e24539c
hoonsdev <123625444+hoonsdev@users.noreply.github.com>
2023-12-05 21:12:17 +0900

    [Silver I] Title: 행렬, Time: 208 ms, Memory: 10264 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.js
@@ -0,0 +1,82 @@
+// 문제 풀이
+function solution(input) {
+  const change = (row, col) => {
+    for (let i = row; i < row + 3; i++) {
+      for (let j = col; j < col + 3; j++) {
+        A[i][j] = 1 - A[i][j];
+      }
+    }
+  };
+  let answer = -1;
+  // 세로, 가로
+  const [N, M] = input
+    .shift()
+    .split(' ')
+    .map((el) => parseInt(el));
+  const A = [];
+  const B = [];
+  for (let i = 0; i < N; i++) {
+    A.push(
+      input
+        .shift()
+        .split('')
+        .map((el) => parseInt(el))
+    );
+  }
+  for (let i = 0; i < N; i++) {
+    B.push(
+      input
+        .shift()
+        .split('')
+        .map((el) => parseInt(el))
+    );
+  }
+  // 부분행렬 최대 크기 3x3
+
+  let count = 0;
+  for (let i = 0; i < N - 2; i++) {
+    for (let j = 0; j < M - 2; j++) {
+      if (A[i][j] === B[i][j]) {
+        continue;
+      } else {
+        change(i, j);
+        count++;
+      }
+    }
+  }
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (A[i][j] !== B[i][j]) {
+        count = -1;
+        break;
+      }
+    }
+  }
+
+  answer = count;
+  console.log(answer);
+}
+
+// Readline module
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+// 이벤트가 발생할 때 실행할 동작을 지정
+// 입력 스트림에 줄바꿈을 나타내는 \n, \r, or \r\n 제어 문자가 나타나거나, 사용자가 Enter 또는 Return을 누를 때 발생
+rl.on('line', (line) => {
+  // readline interface를 통해 다룰 이벤트, 사용자가 콘솔에 입력을 할 때 발생
+  // 엔터 키로 입력받은 여러 줄을 input 배열에 추가 [ [...], [...], ... [...] ] 이런 구조
+  input.push(line);
+});
+
+// Readable 스트림 종료를 제어하는 이벤트
+rl.on('close', () => {
+  solution(input); // 문제 풀이 함수 호출
+  process.exit();
+});
