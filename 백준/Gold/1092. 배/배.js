f61a2ed8e721564ddbfa379034853362f991800e
Taehoon Kim <123625444+hoonsdev@users.noreply.github.com>
2024-05-29 16:42:37 +0900

    [Gold V] Title: 배, Time: 296 ms, Memory: 11276 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.js
+++ 백준/Gold/1092. 배/배.js
@@ -1,7 +1,11 @@
 // 문제 풀이
 function solution(input) {
   const N = +input.shift();
-  const crane = input.shift().split(' ').map(Number);
+  const crane = input
+    .shift()
+    .split(' ')
+    .map(Number)
+    .sort((a, b) => b - a);
   const M = +input.shift();
   const weights = input
     .shift()
@@ -9,22 +13,23 @@ function solution(input) {
     .map(Number)
     .sort((a, b) => b - a);
 
-  const craneMax = Math.max(...crane);
-  const weightMax = weights[0];
-
-  if (craneMax < weightMax) {
+  if (crane[0] < weights[0]) {
     console.log(-1);
     return;
   }
 
+  // 실제 배열 건들지 말고 인덱스로 "접근"만 하기
+  // 크레인을 높은 순으로 정렬했기 때문에 전에 애가 못들면 다음 애도 못 든다.
+  // 그래서 다음 애는 전에 애가 가능한거 이후로부터 탐색하면 됨
   let time = 0;
-  while (weights.length) {
-    for (let i = 0; i < N; i++) {
-      for (let j = 0; j < M; j++) {
-        if (crane[i] >= weights[j]) {
-          weights.splice(j, 1);
-          break;
-        }
+  let completed = 0;
+  while (M > completed) {
+    let cIdx = 0; // crane Idx
+    for (let i = 0; i < M; i++) {
+      if (crane[cIdx] >= weights[i]) {
+        weights[i] = Infinity;
+        cIdx++;
+        completed++;
       }
     }
     time++;
