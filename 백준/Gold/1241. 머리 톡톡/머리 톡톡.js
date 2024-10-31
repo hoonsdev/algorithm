934421d6cded28ef3969e29de276612b8eebbd05
Taehoon Kim <123625444+hoonsdev@users.noreply.github.com>
2024-10-31 23:55:37 +0900

    [Gold V] Title: 머리 톡톡, Time: 3600 ms, Memory: 44280 KB -BaekjoonHub

--- 백준/Gold/1241. 머리 톡톡/머리 톡톡.js
+++ 백준/Gold/1241. 머리 톡톡/머리 톡톡.js
@@ -1,42 +1,31 @@
-function solution(input) {
-	const N = +input[0]
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((str) => parseInt(str));
 
-	const check = new Array(1000001).fill(0)
-	const arr = new Array(N + 1).fill(0)
+const N = +input[0]
 
-	for (let i = 1; i < N + 1; i++) {
-		check[input[i]] += 1 // 해당 수가 몇개 있는지 체크
-		arr[i] = input[i] // 각 사람에 해당하는 수
-	}
+const check = new Array(1000001).fill(0)
+const arr = new Array(N + 1).fill(0)
 
-	const div = (num, ans) => {
-		for (let idx = 1; idx * idx <= num; idx++) {
-			if (num % idx === 0) {
-				ans += idx !== num / idx ? check[num / idx] + check[idx] : check[idx]
-			}
-		}
-		return ans
-	}
-
-	arr.slice(1).map(el => {
-		const answer = div(el, -1)
-		console.log(answer)
-	})
+for (let i = 1; i < N + 1; i++) {
+	check[input[i]] += 1
+	arr[i] = input[i]
 }
 
-const readline = require('readline')
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-})
+const div = (num, ans) => {
+	for (let idx = 1; idx * idx <= num; idx++) {
+		if (num % idx === 0) {
+			ans += idx !== num / idx ? check[num / idx] + check[idx] : check[idx]
+		}
+	}
+	
+	return ans
+}
 
-let input = []
-
-rl.on('line', (line) => {
-	input.push(line)
-})
-
-rl.on('close', () => {
-	solution(input)
-	process.exit()
+arr.slice(1).map(el => {
+	const answer = div(el, -1)
+	console.log(answer)
 })
